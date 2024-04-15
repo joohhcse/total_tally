1e0d947ea7714e6bd450dc7c55655c53560e7eed
hhjoo <joohhcse@gmail.com>
2024-04-15 16:45:14 +0900

    Add flutter locale

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:total_tally/screen/home_screen.dart';
 
 void main() {
@@ -8,6 +9,18 @@ void main() {
     // const MyApp()
     MaterialApp(
       home: HomeScreen(),
+      routes: {'/localization': (context) => Container()},
+      initialRoute: '/localization',
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', ''),
+        Locale('en', ''),
+        Locale('ja', ''),
+      ],
     ),
   );
 }
